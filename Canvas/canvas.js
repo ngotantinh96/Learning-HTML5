bb0bb036311046148ce656c983d659568cd2863c
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-24 17:14:00 +0700

    draw circles and circles animation

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,4 +1,6 @@
 function makeCanvas() {
+  var PI_TWO = Math.PI * 2;
+
   // draw canvas text
   var canvas1 = document.getElementById("canvas1");
   var ctx1 = canvas1.getContext("2d");
@@ -47,7 +49,64 @@ function makeCanvas() {
   ctx3.lineTo(200, 290);
   ctx3.lineTo(290, 290);
   ctx3.lineTo(290, 100);
-  ctx3.moveTo(100, 100);
+  ctx3.lineTo(100, 100);
   ctx3.stroke();
   ctx3.fill();
+  ctx3.closePath();
+
+  // Circles
+  var canvas4 = document.getElementById("canvas4");
+  var ctx4 = canvas4.getContext("2d");
+
+  ctx4.fillStyle = "blue";
+
+  ctx4.beginPath();
+  ctx4.arc(200, 30, 25, 0, PI_TWO);
+  ctx4.fill();
+  ctx4.closePath();
+
+  ctx4.fillStyle = "red";
+
+  ctx4.beginPath();
+  ctx4.arc(200, 100, 45, 0, PI_TWO);
+  ctx4.fill();
+  ctx4.closePath();
+
+  ctx4.fillStyle = "black";
+
+  ctx4.beginPath();
+  ctx4.arc(200, 220, 75, 0, PI_TWO);
+  ctx4.fill();
+  ctx4.closePath();
+
+  //Circles and animation
+  var canvas5 = document.getElementById("canvas5");
+  var ctx5 = canvas5.getContext("2d");
+  var posX = 0;
+  var posY = 0;
+  setInterval(function() {
+	posX++;
+	posY++;
+	
+    ctx5.fillStyle = "black";
+	ctx5.fillRect(0, 0, canvas5.width, canvas5.height);
+	
+    ctx5.fillStyle = "white";
+    ctx5.beginPath();
+    ctx5.arc(posX, 120, 55, 0, PI_TWO);
+    ctx5.fill();
+	ctx5.closePath();
+
+	ctx5.fillStyle = "red";
+    ctx5.beginPath();
+    ctx5.arc(150, posX, 55, 0, PI_TWO);
+    ctx5.fill();
+	ctx5.closePath();
+	
+	ctx5.fillStyle = "blue";
+    ctx5.beginPath();
+    ctx5.arc(350, posX, 55, 0, PI_TWO);
+    ctx5.fill();
+	ctx5.closePath();
+  }, 30);
 }
